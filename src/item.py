5d7374bd649e26dbcb46ded9742858efc85c0bd0
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-09-15 15:15:43 +0500

    домащка_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+from accessify import private
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name=name
-        self.price=price
+        self.price = price
         self.quantity=quantity
         Item.all.append(self)
+        @private
+        def name(self):
+            self.name = name
+
+    @property
+    def get_name(self):
+        return self.name
+    @get_name.setter
+    def set_name(self, name):
+        if len(name ) <=10:
+            self.name =name
+        else:
+            self.name = name[:10]
+
+
 
     def calculate_total_price(self) -> float:
         """
@@ -34,4 +50,11 @@ class Item:
 
 
 item = Item("Смартфон", 10000, 20)
-print(item.apply_discount()  )
+# encode("utf8")
+#print(item.price )
+
+with open('/Users/dinara/PycharmProjects/electronics-shop-project/src/items.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
+
